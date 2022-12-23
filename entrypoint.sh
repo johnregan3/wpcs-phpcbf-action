c20ab1a8752c951c514905c1d66e9198a6fcba7e
John Regan <john.m.regan@gmail.com>
2022-12-23 12:29:32 -0600

    Add PHPCS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 git clone -b master https://github.com/WordPress/WordPress-Coding-Standards.git ~/wpcs
 
+git clone https://github.com/Automattic/VIP-Coding-Standards ${HOME}/vipcs
+git clone https://github.com/sirbrillig/phpcs-variable-analysis ${HOME}/variable-analysis
+${INPUT_PHPCS_BIN_PATH} --config-set installed_paths "${HOME}/wpcs,${HOME}/vipcs,${HOME}/variable-analysis"
+
 if [ -z "${INPUT_EXCLUDES}" ]; then
     EXCLUDES="node_modules,vendor"
 else
